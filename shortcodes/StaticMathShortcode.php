4878e1b207ea3a8469d56ccd41246ea485ee6e88
Ersei Saggi <vcs@ersei.net>
2024-10-30 23:05:57 -0400

    Formatting

--- shortcodes/StaticMathShortcode.php
+++ shortcodes/StaticMathShortcode.php
@@ -1,10 +1,8 @@
 <?php
 
 namespace Grav\Plugin\Shortcodes;
-use Grav\Common\Grav;
 
-use DomainException;
-use Exception;
+use Grav\Common\Grav;
 use Thunder\Shortcode\Shortcode\ShortcodeInterface;
 
 class StaticMathShortcode extends Shortcode
@@ -15,29 +13,33 @@ class StaticMathShortcode extends Shortcode
 
         $rawHandlers->add('tex', function (ShortcodeInterface $sc) {
             $content = $sc->getContent();
-			return $this->render($content);
+
+            return $this->render($content);
         });
         $rawHandlers->add('texi', function (ShortcodeInterface $sc) {
             $content = $sc->getContent();
-			return $this->render($content, true);
+
+            return $this->render($content, true);
         });
     }
 
-	private function render($content, $inline = false) {
-		$mode = $inline ? "inline" : "block";
-		$staticmath_server = Grav::instance()['config']->get('plugins.staticmath.server');
-		$postfield = "mode=" . urlencode($mode) . "&data=" . urlencode($content);
-		$ch = curl_init($staticmath_server);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $postfield);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, [
-			'Content-Length: ' . strlen($postfield)
-		]);
-		$result = curl_exec($ch);
-		if (!$result) {
-			return "<pre>" . $content . "</pre>";
-		}
-		return $result;
-	}
+    private function render($content, $inline = false)
+    {
+        $mode = $inline ? 'inline' : 'block';
+        $staticmath_server = Grav::instance()['config']->get('plugins.staticmath.server');
+        $postfield = 'mode='.urlencode($mode).'&data='.urlencode($content);
+        $ch = curl_init($staticmath_server);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postfield);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Length: '.strlen($postfield),
+        ]);
+        $result = curl_exec($ch);
+        if (! $result) {
+            return '<pre>'.$content.'</pre>';
+        }
+
+        return $result;
+    }
 }
--- staticmath.php
+++ staticmath.php
@@ -7,188 +7,190 @@
  *
  * Based on the code from the Grav MathJax plugin: https://github.com/sommerregen/grav-plugin-mathjax
  *
- * @package		StaticMath
  * @version		2.0.1
+ *
  * @link		<https://sr.ht/~fd/grav-plugin-staticmath>
+ *
  * @author		Ersei Saggi <contact@ersei.net>
  * @copyright	2024, Ersei Saggi
  * @license		<http://opensource.org/licenses/MIT>		MIT
  */
+
 namespace Grav\Plugin;
 
 use Composer\Autoload\ClassLoader;
+use Grav\Common\Grav;
+use Grav\Common\Page\Page;
 use Grav\Common\Plugin;
 use RocketTheme\Toolbox\Event\Event;
-use Grav\Common\Page\Page;
-use Grav\Common\Grav;
 
 /**
  * Class StaticmathPlugin
- * @package Grav\Plugin
  */
 class StaticmathPlugin extends Plugin
 {
+    /**
+     * Instance of StaticMath class
+     *
+     * @var Grav\Plugin\StaticMath
+     */
+    protected $staticmath;
 
-	/**
-	 * Instance of StaticMath class
-	 *
-	 * @var Grav\Plugin\StaticMath
-	 */
-	protected $staticmath;
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *	 that the plugin wants to listen to. The key of each
+     *	 array section is the event that the plugin listens to
+     *	 and the value (in the form of an array) contains the
+     *	 callable (or function) as well as the priority. The
+     *	 higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0],
+            'onGetPageBlueprints' => ['onGetPageBlueprints', 0],
+            'onMarkdownInitialized' => ['onMarkdownInitialized', 0],
+        ];
+    }
 
-	/**
-	 * @return array
-	 *
-	 * The getSubscribedEvents() gives the core a list of events
-	 *	 that the plugin wants to listen to. The key of each
-	 *	 array section is the event that the plugin listens to
-	 *	 and the value (in the form of an array) contains the
-	 *	 callable (or function) as well as the priority. The
-	 *	 higher the number the higher the priority.
-	 */
-	public static function getSubscribedEvents(): array
-	{
-		return [
-			'onPluginsInitialized' => ['onPluginsInitialized', 0],
-			'onGetPageBlueprints' => ['onGetPageBlueprints', 0],
-			'onMarkdownInitialized' => ['onMarkdownInitialized', 0],
-		];
-	}
+    /**
+     * Composer autoload
+     */
+    public function autoload(): ClassLoader
+    {
+        return require __DIR__.'/vendor/autoload.php';
+    }
 
-	/**
-	 * Composer autoload
-	 *
-	 * @return ClassLoader
-	 */
-	public function autoload(): ClassLoader
-	{
-		return require __DIR__ . '/vendor/autoload.php';
-	}
+    /**
+     * Register shortcodes
+     */
+    public function onShortcodeHandlers()
+    {
+        $this->grav['shortcode']->registerAllShortcodes(__DIR__.'/shortcodes');
+    }
 
-	/**
-	 * Register shortcodes
-	 */
-	public function onShortcodeHandlers()
-	{
-		$this->grav['shortcode']->registerAllShortcodes(__DIR__ . '/shortcodes');
-	}
+    /**
+     * Initialize the plugin
+     */
+    public function onPluginsInitialized(): void
+    {
+        // Don't proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            $this->active = false;
 
-	/**
-	 * Initialize the plugin
-	 */
-	public function onPluginsInitialized(): void
-	{
-		// Don't proceed if we are in the admin plugin
-		if ($this->isAdmin()) {
-			$this->active = false;
-			return;
-		}
+            return;
+        }
 
-		$this->enable([
-			'onShortcodeHandlers' => ['onShortcodeHandlers', 0],
-			'onPageInitialized' => ['onPageInitialized', 0]
-		]);
-	}
+        $this->enable([
+            'onShortcodeHandlers' => ['onShortcodeHandlers', 0],
+            'onPageInitialized' => ['onPageInitialized', 0],
+        ]);
+    }
 
-	public function onMarkdownInitialized(Event $event)
-	{
-		$markdown = $event['markdown'];
+    public function onMarkdownInitialized(Event $event)
+    {
+        $markdown = $event['markdown'];
 
-		$page = $this->grav['page'];
-		$config = $this->mergeConfig($page);
-		if (!($config->get('enabled') && $config->get('active'))) {
-			return;
-		}
-		$markdown->addBlockType('$', 'Staticmath', true, false);
-		$markdown->addInlineType('$', 'Staticmath');
+        $page = $this->grav['page'];
+        $config = $this->mergeConfig($page);
+        if (! ($config->get('enabled') && $config->get('active'))) {
+            return;
+        }
+        $markdown->addBlockType('$', 'Staticmath', true, false);
+        $markdown->addInlineType('$', 'Staticmath');
 
-		$markdown->blockStaticmath = function($Line) {
-			if (preg_match('/^\$\$$/', $Line['text'], $matches)) {
-				$Block = [
-					'element' => [
-						'name' => 'div',
-						'handler' => 'lines',
-						'text' => [],
-					],
-				];
+        $markdown->blockStaticmath = function ($Line) {
+            if (preg_match('/^\$\$$/', $Line['text'], $matches)) {
+                $Block = [
+                    'element' => [
+                        'name' => 'div',
+                        'handler' => 'lines',
+                        'text' => [],
+                    ],
+                ];
 
-				return $Block;
-			}
-		};
+                return $Block;
+            }
+        };
 
-		$markdown->blockStaticmathContinue = function($Line, array $Block) {
-			if (isset($Block['interrupted'])) {
-				return;
-			}
-			if (!preg_match('/^\$\$$/', $Line['text'])) {
-				$Block['element']['text'][] = $Line['text'];
-			} else {
-				$text = implode(
-					"\n",
-					$Block['element']['text']
-				);
+        $markdown->blockStaticmathContinue = function ($Line, array $Block) {
+            if (isset($Block['interrupted'])) {
+                return;
+            }
+            if (! preg_match('/^\$\$$/', $Line['text'])) {
+                $Block['element']['text'][] = $Line['text'];
+            } else {
+                $text = implode(
+                    "\n",
+                    $Block['element']['text']
+                );
 
-				$Block['element']['text'] = (array) $this->render($text);
-			}
-			return $Block;
-		};
+                $Block['element']['text'] = (array) $this->render($text);
+            }
 
-		$markdown->inlineStaticmath = function($Line) {
-			if (preg_match('/\$(.+?)\$/', $Line['text'], $matches)) {
-				$Block = [
-					'extent' => strlen($matches[0]),
-					'element' => [
-						'name' => 'span',
-						'handler' => 'lines',
-						'text' => (array) $this->render($matches[1], true),
-					],
-				];
-				return $Block;
-			}
-		};
-	}
+            return $Block;
+        };
 
-	/**
-	 * Initialize Twig configuration and filters.
-	 */
-	public function onPageInitialized()
-	{
-		/** @var Page $page */
-		$page = $this->grav['page'];
+        $markdown->inlineStaticmath = function ($Line) {
+            if (preg_match('/\$(.+?)\$/', $Line['text'], $matches)) {
+                $Block = [
+                    'extent' => strlen($matches[0]),
+                    'element' => [
+                        'name' => 'span',
+                        'handler' => 'lines',
+                        'text' => (array) $this->render($matches[1], true),
+                    ],
+                ];
 
-		// Skip if active is set to false
-		$config = $this->mergeConfig($page);
-		if (!($config->get('enabled') && $config->get('active'))) {
-			return;
-		}
+                return $Block;
+            }
+        };
+    }
 
-		if ($this->config->get('plugins.staticmath.built_in_css')) {
-			$this->grav['assets']->add('plugins://staticmath/assets/css/Temml-Latin-Modern.css');
-		}
-	}
+    /**
+     * Initialize Twig configuration and filters.
+     */
+    public function onPageInitialized()
+    {
+        /** @var Page $page */
+        $page = $this->grav['page'];
 
-	public function onGetPageBlueprints($event)
-	{
-		$types = $event->types;
-		$types->scanBlueprints('plugin://staticmath/blueprints');
-	}
+        // Skip if active is set to false
+        $config = $this->mergeConfig($page);
+        if (! ($config->get('enabled') && $config->get('active'))) {
+            return;
+        }
 
-	private function render($content, $inline = false) {
-		$mode = $inline ? "inline" : "block";
-		$staticmath_server = Grav::instance()['config']->get('plugins.staticmath.server');
-		$postfield = "mode=" . urlencode($mode) . "&data=" . urlencode($content);
-		$ch = curl_init($staticmath_server);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $postfield);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, [
-			'Content-Length: ' . strlen($postfield)
-		]);
-		$result = curl_exec($ch);
-		if (!$result) {
-			return "<pre>" . $content . "</pre>";
-		}
-		return $result;
-	}
+        if ($this->config->get('plugins.staticmath.built_in_css')) {
+            $this->grav['assets']->add('plugins://staticmath/assets/css/Temml-Latin-Modern.css');
+        }
+    }
 
+    public function onGetPageBlueprints($event)
+    {
+        $types = $event->types;
+        $types->scanBlueprints('plugin://staticmath/blueprints');
+    }
+
+    private function render($content, $inline = false)
+    {
+        $mode = $inline ? 'inline' : 'block';
+        $staticmath_server = Grav::instance()['config']->get('plugins.staticmath.server');
+        $postfield = 'mode='.urlencode($mode).'&data='.urlencode($content);
+        $ch = curl_init($staticmath_server);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postfield);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Length: '.strlen($postfield),
+        ]);
+        $result = curl_exec($ch);
+        if (! $result) {
+            return '<pre>'.$content.'</pre>';
+        }
+
+        return $result;
+    }
 }
